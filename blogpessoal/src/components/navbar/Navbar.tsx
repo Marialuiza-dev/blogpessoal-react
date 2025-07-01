6d0f4787d39bb2af02e966db255c5c6cf96f589f
Maria Luiza <maluduartetorres@gmail.com>
2025-07-01 18:32:41 -0300

    feat: sistema de rotas

--- blogpessoal/src/components/navbar/Navbar.tsx
+++ blogpessoal/src/components/navbar/Navbar.tsx
@@ -21,7 +21,6 @@ function Navbar() {
             
                 <div className="container flex justify-between text-lg mx-8">
                     <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
-
                     <div className='flex gap-4'>
                         Postagens
                         Temas
